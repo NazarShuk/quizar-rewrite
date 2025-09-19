1c3bfd7df72fd2f95afdf618c7aa2b1dd28c2759
NshIS <82755033+NazarShuk@users.noreply.github.com>
2025-09-19 13:15:19 +0000

    i love clerk

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import '../app.css';
 	import favicon from '$lib/assets/favicon.svg';
+	import { ClerkProvider, SignInButton, SignedIn, SignedOut, UserButton } from 'svelte-clerk';
 
 	let { children, data } = $props();
 </script>
@@ -9,11 +10,19 @@
 	<link rel="icon" href={favicon} />
 </svelte:head>
 
-<div class="flex h-screen w-full flex-col">
-	<header class="mr-auto ml-auto flex h-8 w-[90%] items-center justify-between">
-		<div class="flex items-center gap-2">
-			<a href="/" class="text-xl font-bold">Quizar</a>
-		</div>
-	</header>
-	{@render children?.()}
-</div>
+<ClerkProvider>
+	<div class="flex h-screen w-full flex-col">
+		<header class="mr-auto ml-auto flex h-12 w-[90%] items-center justify-between">
+			<div class="flex items-center gap-2">
+				<a href="/" class="text-xl font-bold">Quizar</a>
+			</div>
+			<SignedOut>
+				<SignInButton />
+			</SignedOut>
+			<SignedIn>
+				<UserButton />
+			</SignedIn>
+		</header>
+		{@render children?.()}
+	</div>
+</ClerkProvider>
