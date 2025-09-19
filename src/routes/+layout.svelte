3f23ba5bccc8dc4f85f1d350225d14229a815e45
NshIS <82755033+NazarShuk@users.noreply.github.com>
2025-09-19 13:09:06 +0000

    switched to svelte clerk because im so done

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,6 @@
 <script lang="ts">
 	import '../app.css';
 	import favicon from '$lib/assets/favicon.svg';
-	import { SignIn } from '@auth/sveltekit/components';
-	import { Button } from '$lib/components/ui/button';
-	import { signIn } from '@auth/sveltekit/client';
 
 	let { children, data } = $props();
 </script>
@@ -17,9 +14,6 @@
 		<div class="flex items-center gap-2">
 			<a href="/" class="text-xl font-bold">Quizar</a>
 		</div>
-		{#if !data.session?.user}
-			<a href="/signin">Sign in</a>
-		{/if}
 	</header>
 	{@render children?.()}
 </div>
