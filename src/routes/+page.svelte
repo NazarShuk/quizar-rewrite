f83f8f8571bf82da392494d412f92a3e5529de44
NshIS <82755033+NazarShuk@users.noreply.github.com>
2025-09-22 12:18:37 +0000

    main page update

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,11 +2,6 @@
 	import { goto } from '$app/navigation';
 	import Button from '$lib/components/ui/button/button.svelte';
 	import { resolve } from '$app/paths';
-
-	const quotes = [
-		"I hate that one website that's so evil bru",
-		'yo dawg i need to study for this test real quick'
-	];
 </script>
 
 <svelte:head>
@@ -14,11 +9,32 @@
 </svelte:head>
 
 <div class="flex h-screen w-full flex-col items-center justify-center">
-	<h1 class="mb-5 text-8xl font-bold">Quizar</h1>
-	<h2 class="italic">{quotes[Math.floor(Math.random() * quotes.length)]}</h2>
+	<h1
+		class="mb-5 bg-gradient-to-br from-primary to-accent bg-clip-text text-8xl font-bold text-transparent"
+	>
+		Quizar
+	</h1>
 
-	<div class="mt-10 flex flex-row gap-4">
+	<div class="flex flex-row gap-4">
 		<Button onclick={() => goto(resolve('/search'))}>Explore Study Sets</Button>
 		<Button onclick={() => goto(resolve('/new'))}>Make your own</Button>
+	</div>
+	<div class="mt-10 h-fit w-[90%] lg:w-[50%]">
+		<p class="text-center">
+			Quizar is a tool to help you prepare for your upcoming quizes or tests. You can create your
+			own study sets that consist of flashcards and use different modes to study.
+		</p>
+		<p class="mt-5 text-center">
+			Quizar will forever be free and open source. I believe that everyone should be allowed to
+			learn without paywalls. <span class="text-xs italic">unlike some other evil companies</span>
+		</p>
+		<p class="flex flex-row justify-between gap-2">
+			<a href="https://github.com/NazarShuk/quizar-rewrite" target="_blank" class="text-primary">
+				<span class="text-xs">View on GitHub</span>
+			</a>
+			<a href="https://www.nshis.com" target="_blank" class="text-primary">
+				<span class="text-xs">Made by NshIS</span>
+			</a>
+		</p>
 	</div>
 </div>
